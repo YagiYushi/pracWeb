8f8ee5e23004b32d12f7a0d554f537be58b4639a
cowgoat <kuroyaginomori@gmail.com>
2021-12-05 17:58:37 +0900

    [Add] top page and index page

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
-connection.connect((err) => {
-  if (err) {
-    console.log('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('success');
-});
+
 
 app.get('/', (req, res) => {
   res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+    res.render('index.ejs');
 });
 
 app.listen(3000);
